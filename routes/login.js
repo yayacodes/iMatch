538d7c797f8e98043e504e3037d5ac9089dbdd73
HienBui711 <hbui@stevens.edu>
2019-12-14 18:27:16 -0500

    added POST request function in login.js

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const userMethods = require("../data/users.js");
 
 router.get('/', async (req, res) => {
     try {
@@ -10,4 +11,30 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.post('/login', async (req,res) => {
+    if (!req.body.username || !req.body.password) {
+        res.status(401).render('user/login',{ error: "Please provide both a valid username and a valid password"});
+        return;
+    }
+    try {
+        const inputUser = await userMethods.getUserByUsername(req.body.username);
+        
+        if (!inputUser) {
+            res.status(401).render('user/login', { error: "No user with that username"});
+            return;
+        }
+        
+        let verifyUser = await userMethods.verifyUser(req.body.username, req.body.password, req.session.id);
+        
+        if (verifyUser) {
+            req.session.loginStatus = true;
+            res.redirect('/dashboard');
+        } else {
+            res.status(401).render("user/login", { error: "Cannot verify user"});
+            return;
+        }
+    } catch (e) {
+        res.status(500).send();
+    }
+});
 module.exports = router;
