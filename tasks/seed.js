31474ea1b4f6a8c4de2d97b091dec9a37e97d1c5
Yehia Awad <yehiakawad@gmail.com>
2019-12-16 09:36:23 -0500

    fixed seeding issue

--- tasks/seed.js
+++ tasks/seed.js
@@ -14,6 +14,7 @@ async function main() {
       lastname: "lastname1",
       email: "email1@gmail.com",
       phone: "111-123-4567",
+      role: "student",
       zipcode: "07030",
       latitude: "40.744052",
       longitude: "-74.0270745",
@@ -27,6 +28,7 @@ async function main() {
       lastname: "lastname2",
       email: "email2@gmail",
       phone: "222-123-4567",
+      role: "student",
       zipcode: "07030",
       latitude: "40.744052",
       longitude: "-74.0270745",
@@ -40,6 +42,7 @@ async function main() {
       lastname: "lastname3",
       email: "email3@gmail.com",
       phone: "333-123-4567",
+      role: "student",
       zipcode: "90017",
       latitude: "34.054379",
       longitude: "-118.267281",
@@ -53,6 +56,7 @@ async function main() {
       lastname: "lastname4",
       email: "email4@gmail.com",
       phone: "444-123-4567",
+      role: "student",
       zipcode: "19107",
       latitude: "39.950802",
       longitude: "-75.160118",
@@ -66,6 +70,7 @@ async function main() {
       lastname: "lastname5",
       email: "email5@gmail.com",
       phone: "555-123-4567",
+      role: "student",
       zipcode: "07030",
       latitude: "40.744052",
       longitude: "-74.0270745",
@@ -79,18 +84,19 @@ async function main() {
       lastname: "lastname6",
       email: "email6@gmail.com",
       phone: "666-123-4567",
+      role: "student",
       zipcode: "07030",
       latitude: "40.744052",
       longitude: "-74.0270745",
       availability: ['Tuesday', 'Thursday', 'Friday']
   } 
    
-  await users.create(user1.username, user1.password, user1.firstname, user1.lastname, user1.email, user1.phone, user1.zipcode, user1.latitude, user1.longitude, user1.availability);
-  await users.create(user2.username, user2.password, user2.firstname, user2.lastname, user2.email, user2.phone, user2.zipcode, user2.latitude, user2.longitude, user2.availability);
-  await users.create(user3.username, user3.password, user3.firstname, user3.lastname, user3.email, user3.phone, user3.zipcode, user3.latitude, user3.longitude, user3.availability);
-  await users.create(user4.username, user4.password, user4.firstname, user4.lastname, user4.email, user4.phone, user4.zipcode, user4.latitude, user4.longitude, user4.availability);
-  await users.create(user5.username, user5.password, user5.firstname, user5.lastname, user5.email, user5.phone, user5.zipcode, user5.latitude, user5.longitude, user5.availability);
-  await users.create(user6.username, user6.password, user6.firstname, user6.lastname, user6.email, user6.phone, user6.zipcode, user6.latitude, user6.longitude, user6.availability);
+  await users.create(user1.username, user1.password, user1.firstname, user1.lastname, user1.email, user1.phone, user1.role, user1.zipcode, user1.latitude, user1.longitude, user1.availability);
+  await users.create(user2.username, user2.password, user2.firstname, user2.lastname, user2.email, user2.phone, user2.role,user2.zipcode, user2.latitude, user2.longitude, user2.availability);
+  await users.create(user3.username, user3.password, user3.firstname, user3.lastname, user3.email, user3.phone, user3.role,user3.zipcode, user3.latitude, user3.longitude, user3.availability);
+  await users.create(user4.username, user4.password, user4.firstname, user4.lastname, user4.email, user4.phone, user4.role,user4.zipcode, user4.latitude, user4.longitude, user4.availability);
+  await users.create(user5.username, user5.password, user5.firstname, user5.lastname, user5.email, user5.phone, user5.role,user5.zipcode, user5.latitude, user5.longitude, user5.availability);
+  await users.create(user6.username, user6.password, user6.firstname, user6.lastname, user6.email, user6.phone, user6.role,user6.zipcode, user6.latitude, user6.longitude, user6.availability);
 
   const course1 = {
     name: "web programming",
